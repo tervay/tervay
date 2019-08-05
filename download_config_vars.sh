3a646c6c6f0adcf79db4b9b4455d8cc0f69b9ba4
Justin Tervay <justin@tervay.com>
2019-08-05 17:45:19 -0400

    Initial commit
    Add gunicorn
    
    heroku conf
    
    change runtime to 3.7.3
    
    Hotlink basics
    
    Streamline build

---
+++ download_config_vars.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+heroku config:get AUTH -s >> .env
+heroku config:get DATABASE_URL -s >> .env
+heroku config:get SALT -s >> .env
+heroku config:get DB_HOST -s >> .env
+heroku config:get DB_DATABASE -s >> .env
+heroku config:get DB_USER -s >> .env
+heroku config:get DB_PASSWORD -s >> .env
