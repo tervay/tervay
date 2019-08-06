514ff6d2c2ec2b6e6b77d4872ea9e18e1a304167
Justin Tervay <justin@tervay.com>
2019-08-05 22:26:58 -0400

    add sslify

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from flask_sslify import SSLify
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = os.environ.get('CSRF_KEY') or 'testing-string'
+SSLify(app, subdomains=True)
