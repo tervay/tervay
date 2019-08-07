2892a1f172b00141d41110728e9d91036cbd8849
Justin Tervay <justin@tervay.com>
2019-08-06 21:19:52 -0400

    Lol

--- webpy/functions/test.py
+++ webpy/functions/test.py
@@ -11,3 +11,12 @@ def add(a: int, b: int):
         return r, cache_hit
 
     return a + b
+
+
+@expose(name='Idiot Calculator', url='_idiot')
+def idiot(This_Person_Is_An_Idiot: str):
+    r, cache_hit = cache_frame(inspect.currentframe())
+    if r is not None:
+        return r, cache_hit
+
+    return 'Jack is an idiot'
